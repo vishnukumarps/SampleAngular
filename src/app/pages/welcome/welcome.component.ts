339e6b547eb203878c6e8783668490a7ad9106da
vishnukumar.ps@outlook.com <vishnukumar.ps@outlook.com>
2021-05-08 08:17:04 +0530

    commit

--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -7,8 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WelcomeComponent implements OnInit {
   condition1 = false;
+  isVisible = false;
   a = 1;
   students = ['Vishnu', 'Gopu', 'Reshma'];
+
+  employees = [
+    {
+      name: 'Vishnu',
+      place: 'Alappuzha',
+      mob: '7736177715'
+    },
+    {
+      name: 'Reshma',
+      place: 'Calicut',
+      mob: '7736177715'
+    },
+    {
+      name: 'Gopu',
+      place: 'Kollam',
+      mob: '7736177715'
+    },
+
+
+  ];
+
+  show() {
+    this.isVisible = true;
+  }
+
+  hide() {
+    this.isVisible = false;
+  }
   constructor() { }
 
   ngOnInit() {
